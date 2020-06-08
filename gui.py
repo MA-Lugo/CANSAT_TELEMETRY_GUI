c04afee4fd7a50dacbb36d7f58937e38ec36c64a
Mario Lugo U <mariolg2404@gmail.com>
2020-06-08 03:43:37 -0500

    Se mapearon todas las variables,se incluyo barras de estado para datos de telemetria,se hizo conversion de presion a altitud y las coordenadas de DMS a DD, se incorporó un boton que redirige a googlemaps, ademas se incluyen los fonts utilizados

--- gui.py
+++ gui.py
@@ -10,21 +10,33 @@ import tkinter.font as tkFont
 import serial
 import os
 import webbrowser
+import math  
 
 
-
-
+b = 0.0000225577
+k = 5.2559
 
 serial_data     = ''
 raw_serial = None
 filter_data = None
 sat_stream1 = None
 sat_stream2 = None
-update_period = 2
+update_period = 3
 serial_object = None
 power = None
 batt_percent = None
 packages    = 0
+sea_altitude = 0.0
+altitude = 0.0
+#LINK
+LINK_LAT = None
+LINK_LONG = None
+
+#CONSTANTS
+BATTERY_MIN = 2.9
+BATTERY_MAX = 4.1
+
+
 
 #COLORS
 color_gui_bg   = "#918D9E" 
@@ -41,11 +53,11 @@ color_entry_bg    = "#AB9FAB"
 
 color_probar_tg = "#B3A7B3"
 color_batt_bar  = "#18A367"
-color_temp_bar  = "#FF6666"
-color_rh_bar    = "#709BFF"
-color_pess_bar  = "#1C8C7B"
-color_salt_bar  = "#D9754C"
-color_alt_bar   = "#D9BD57"
+color_temp_bar  = "#D0363E"
+color_rh_bar    = "#4999AB"
+color_pess_bar  = "#3F8C54"
+color_salt_bar  = "#2E606B"
+color_alt_bar   = "#59433A"
 
 
 GUI = Tk()
@@ -56,9 +68,10 @@ GUI.configure(bg = color_gui_bg)
 #FONTS
 font_frame_name = tkFont.Font(family="Alte Haas Grotesk" ,size=8,weight ="bold")
 font_boldunder = tkFont.Font(family="Alte Haas Grotesk" ,size=9,weight ="bold",underline = 1)
-font_data_1 = tkFont.Font(family="Alte Haas Grotesk" ,size=9)
+font_data_1 = tkFont.Font(family="Alte Haas Grotesk" ,size=10)
 font_terminal_txt = tkFont.Font(family="Envy Code R" ,size=11)
 font_con_stat = tkFont.Font(family="Alte Haas Grotesk" ,size=11,weight ="bold")
+font_var_bold = tkFont.Font(family="Alte Haas Grotesk" ,size=11,weight ="bold")
 
 #STYLES
 ProgressBar_style = ttk.Style()
@@ -73,6 +86,24 @@ ProgressBar_style.configure("alt.Vertical.TProgressbar", troughcolor=color_proba
 conn_var    = StringVar()
 disconn_var = StringVar()
 disconn_var.set("DISCONNECTED")
+
+GPS_DataTime = StringVar()
+GPS_Satellites = StringVar()
+GPS_Lat = StringVar()
+GPS_Lon = StringVar()
+
+Tel_Temp1   = StringVar()
+Tel_Temp2   = StringVar()
+Tel_RH      = StringVar()
+Tel_Press   = StringVar()
+Tel_SAlt    = StringVar()
+Tel_Alt     = StringVar()
+
+
+Batt_Volt   = StringVar()
+Batt_Curr   = StringVar()
+Batt_Power  = StringVar()
+Batt_Perc   = StringVar()
 
 def connect():
     """Esta funcion inicia la conexion al dispositivo UART
@@ -89,12 +120,12 @@ def connect():
     try:
         if version_ == 2:
             try:
-                serial_object = serial.Serial('/dev/tty' + str(port), baudrate =baud, timeout = .05)
+                serial_object = serial.Serial('/dev/tty' + str(port), baudrate =baud, timeout = 1)
             
             except:
                 print ("Cant Open Specified Port")
         elif version_ == 1:
-            serial_object = serial.Serial( str(port), baudrate= baud, timeout = .05)
+            serial_object = serial.Serial( str(port), baudrate= baud, timeout = 1)
         elif version_ == 0:
             print ("seleccione SO")
 
@@ -141,15 +172,15 @@ def get_data():
                 data_flow.insert(END,raw_serial)
                 data_flow.see(END)
                 
-
-                filter_data = raw_serial.replace("\n","")
+                filter_data = raw_serial.replace("\n","").replace("\r","").replace("\n\r","")
+               
+                
                 if filter_data.startswith("#CAN"):
                     sat_stream1 = filter_data.replace("#CAN,","").split(",")
                     print(sat_stream1)
-                
-                    
-                elif filter_data.startswith("\r#SAT"):
-                    sat_stream2 = filter_data.replace("\r#SAT,","").split(",")
+                                 
+                if filter_data.startswith("#SAT"):
+                    sat_stream2 = filter_data.replace("#SAT,","").split(",")
                     print(sat_stream2)
                                                                 
             else:
@@ -174,6 +205,82 @@ def get_data():
             e=1
 
 
+def update_gui():
+
+    global sat_stream1
+    global sat_stream2
+    global LINK_LAT
+    global LINK_LONG
+    global packages
+
+    while(1):
+        if sat_stream1 != None:
+            try:
+                if(sat_stream1[0] != ""):
+                    GPS_DataTime.set(strftime("%d/%m/%y")+"  |  "+sat_stream1[0]+":"+sat_stream1[1]+":"+sat_stream1[2])
+                    
+                    LAT = sat_stream1[3].replace("S","-").replace("N","")
+                    LAT = float(LAT)/100
+                    LAT = round(int(LAT) + ((LAT-int(LAT))*(5/3)),7)
+
+                    LONG = sat_stream1[4].replace("W","-").replace("E","")
+                    LONG = float(LONG)/100
+                    LONG = round(int(LONG) + ((LONG-int(LONG))*(5/3)),7)
+
+                    GPS_Lat.set(LAT)
+                    GPS_Lon.set(LONG)
+                    LINK_LAT = str(LAT)
+                    LINK_LONG = str(LONG)
+                    GPS_Satellites.set(sat_stream1[5])
+                    
+                
+            except:
+                print("stream1 Err")
+            sat_stream1 = None
+            
+        if sat_stream2 != None:
+            try:
+                Tel_Temp1.set(sat_stream2[0])
+                Tel_RH.set(sat_stream2[1])
+                Tel_Temp2.set(sat_stream2[2])
+                Tel_Press.set(sat_stream2[3])        
+                Batt_Volt.set(sat_stream2[5] + " V")
+                Batt_Curr.set(sat_stream2[6] + " mA")
+            except:
+                print("stream2 Err")  
+            try:
+                power = ((float(sat_stream2[5])*1000)* float(sat_stream2[6]))/1000
+                Batt_Power.set(str(round(power,2)) + " mW")
+
+                batt_percent = ((float(sat_stream2[5]) - BATTERY_MIN)*100)/(BATTERY_MAX-BATTERY_MIN)
+                Batt_Perc.set(str(round(batt_percent)) + " %")
+
+                sea_altitude = round(((1-((float(sat_stream2[3])) / 1013.25)**(1/k)) / b),2)
+                Tel_SAlt.set(str(sea_altitude) + " m")
+
+                altitude = round(float(sat_stream2[3]) + float(sat_stream2[4]),2)
+                altitude = round(sea_altitude - (round(((1-((altitude) / 1013.25)**(1/k)) / b),2)),2)
+                Tel_Alt.set(str(altitude) + " m")
+
+            except:
+                print("Math Err")
+            try:
+                temperature_bar1["value"] = sat_stream2[0]
+                humidity_bar["value"] = sat_stream2[1]
+                temperature_bar2["value"] = sat_stream2[2]
+                pressure_bar["value"] = sat_stream2[3]
+                battery_bar["value"] = batt_percent
+                salt_bar["value"] = sea_altitude
+                alt_bar["value"] = altitude
+            except:
+                print("PBars Err")
+            sat_stream2 = None   
+        
+        time.sleep(.001)
+
+
+
+
 def disconnect():
     conn_var.set("")
     disconn_var.set("DISCONNECTED")
@@ -193,6 +300,9 @@ def send():
         print ("Sent Nothing")
     
     serial_object.write(send_data.encode('utf-8'))
+
+def gmaps():
+    webbrowser.open("https://www.google.com/maps/search/?api=1&query="+ LINK_LAT + "," + LINK_LONG)
 
 if __name__ == "__main__":
     
@@ -234,6 +344,10 @@ if __name__ == "__main__":
     data_flow = Text(width = 59, height = 6,bg=color_terminal_bg,fg =color_terminal_text,font = font_terminal_txt)
     data_flow.place(x = 20, y = 26)
 
+    t2 = threading.Thread(target = update_gui)
+    t2.daemon = True
+    t2.start()
+
     ######FRAME LABELS
     Label(text = "Terminal",bg = color_gui_bg,fg = color_label_txt,font = font_frame_name).place(x = 20, y =8)
     Label(text = "Battery Status",bg = color_gui_bg,fg = color_label_txt,font = font_frame_name).place(x = 530, y =8)
@@ -251,9 +365,13 @@ if __name__ == "__main__":
     Label(text = "Remaining Percent:",bg = color_gui_bg,fg = color_label_txt,font = ("Alte Haas Grotesk" ,8)).place(x = 570, y =100)
     #TELEMETRY LAB
     Label(text = "DH22 | Temperature",font = font_data_1,bg = color_frame_bg,fg = color_label_txt).place(x = 92, y= 195,anchor= "center")
+    Label(text = "°C" ,font = ("DS-Digital",15),bg = color_frame_bg,fg = color_label_txt).place(x = 135, y= 225,anchor= "center")
     Label(text = "BME | Temperature",font = font_data_1,bg = color_frame_bg,fg = color_label_txt).place(x = 237, y= 195,anchor= "center")
+    Label(text = "°C" ,font = ("DS-Digital",15),bg = color_frame_bg,fg = color_label_txt).place(x = 280, y= 225,anchor= "center")
     Label(text = "R. Humidity",font = font_data_1,bg = color_frame_bg,fg = color_label_txt).place(x = 92, y= 300,anchor= "center")
+    Label(text = "%" ,font = ("DS-Digital",18),bg = color_frame_bg,fg = color_label_txt).place(x = 135, y= 330,anchor= "center")
     Label(text = "Pressure:",font = font_data_1,bg = color_frame_bg,fg = color_label_txt).place(x = 237, y= 300,anchor= "center")
+    Label(text = "hp" ,font = ("DJB Get Digital" ,13),bg = color_frame_bg,fg = color_label_txt).place(x = 290, y= 330,anchor= "center")
     Label(text = "A. Altitude:",font = font_data_1,bg = color_frame_bg,fg = color_label_txt).place(x = 370, y= 350,anchor= "center")
     Label(text = "Altitude:",font = font_data_1,bg = color_frame_bg,fg = color_label_txt).place(x = 450, y= 350,anchor= "center")
     #GPS DATA LAB
@@ -269,6 +387,29 @@ if __name__ == "__main__":
     Label(textvariable = disconn_var,bg = color_gui_bg,fg = "#F20F0F",font = font_con_stat).place(x = 21, y =555)
     Label(textvariable = conn_var,bg = color_gui_bg,fg = "#13F235",font = font_con_stat).place(x = 20, y =555)
     
+    ######VARIABLE LABEL
+    #BATTERY
+    Label(textvariable = Batt_Volt,font = font_var_bold,bg = color_gui_bg,fg = color_label_txt).place(x = 585, y= 40)
+    Label(textvariable = Batt_Curr,font = font_var_bold,bg = color_gui_bg,fg = color_label_txt).place(x = 695, y= 40)
+    Label(textvariable = Batt_Power,font = font_var_bold,bg = color_gui_bg,fg = color_label_txt).place(x = 580, y= 70)
+    Label(textvariable = Batt_Perc,font = font_var_bold,bg = color_gui_bg,fg = color_label_txt).place(x = 675, y= 95)
+    #TELEMETRY 
+    Label(textvariable = Tel_Temp1,font = "DS-Digital 24 bold",bg = color_frame_bg,fg = color_label_txt).place(x = 60, y= 210)
+    Label(textvariable = Tel_Temp2,font = "DS-Digital 24 bold",bg = color_frame_bg,fg = color_label_txt).place(x = 190, y= 210)
+    Label(textvariable = Tel_RH,font = "DS-Digital 24 bold",bg = color_frame_bg,fg = color_label_txt).place(x = 92, y= 330,anchor= "center")
+    Label(textvariable = Tel_Press,font = "DS-Digital 20 bold",bg = color_frame_bg,fg = color_label_txt).place(x = 237, y= 330,anchor= "center")
+    Label(textvariable = Tel_SAlt,font = ("DJB Get Digital",15),bg = color_frame_bg,fg = color_label_txt).place(x = 370, y= 370,anchor= "center")
+    Label(textvariable = Tel_Alt,font = ("DJB Get Digital",15),bg = color_frame_bg,fg = color_label_txt).place(x = 450, y= 370,anchor= "center")
+
+    #GPS DATA
+    Label(textvariable = GPS_DataTime,bg = color_gui_bg,fg = color_label_txt,font = font_var_bold).place(x = 650, y =200, anchor= "center")
+    Label(textvariable = GPS_Satellites,bg = color_gui_bg,fg = color_label_txt,font = font_var_bold).place(x = 650, y =250, anchor= "center")
+    Label(textvariable = GPS_Lat,bg = color_gui_bg,fg = color_label_txt,font = font_var_bold).place(x = 650, y =300)
+    Label(textvariable = GPS_Lon,bg = color_gui_bg,fg = color_label_txt,font = font_var_bold).place(x = 645, y =320)
+
+
+
+
     ######ENTRY
     send_data_entry = Entry(width = 7,bg = color_entry_bg, fg = color_terminal_bg)
     send_data_entry.place(x = 360, y = 485)
@@ -290,7 +431,8 @@ if __name__ == "__main__":
     Button(text = "Send",font = ("Alte Haas Grotesk",8), command = send, width = 6,bg=color_button_bg,fg = color_button_txt).place(x = 430, y = 483)
     Button(text = "Connect",font = ("Alte Haas Grotesk",8),command = connect,width= 9,bg=color_button_bg,fg = color_button_txt).place(x = 150, y = 483)
     Button(text = "Disconnect",font = ("Alte Haas Grotesk",8), command = disconnect,width= 9,bg=color_button_bg,fg = color_button_txt).place(x =250, y = 483)
-    
+    Button(text = "Google Maps", command = gmaps, width =15 ,bg=color_mapsbtn_bg,fg = color_button_txt,font = ("Alte Haas Grotesk",12)).place(x = 650, y = 370,anchor = "center")
+
 
     ######PROGRESS BAR
     battery_bar = ttk.Progressbar(style="battery.Horizontal.TProgressbar",orient = HORIZONTAL, mode = 'determinate', length = 240, max = 100)
